91103836321ec31bac734318f7de40c449ac8a47
Teguh Rianto <teguh.rianto22@gmail.com>
2020-04-03 03:53:24 +0700

    update footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,2 +1,236 @@
-<h1>Footer</h1>
+<footer class="footer">
+    <div class="container">
+        <div class="footer__top">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Hubungi Kami
+                        </h6>
+                        <p class="mb-4">
+                            <a href="tel:02745305505">0274-5305505</a><br>
+                            Senin - Minggu<br>
+                            24 Jam Nonstop
+                        </p>
+                        <p>
+                            Jl. Selokan Mataram Monjali<br>
+                            Karangjati MT I/304<br>
+                            Sinduadi, Mlati, Sleman<br>
+                            Yogyakarta 55284
+                        </p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Layanan
+                        </h6>
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0)">Domain</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Shared Hosting</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Cloud VPS Hosting</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Managed VPS Hosting</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Web Builder</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Keamanan SSL/HTTPS</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Jasa Pembuatan Website</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Program Afiliasi</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Service Hosting
+                        </h6>
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0)">Hosting Murah</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Hosting Indonesia</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Hosting Singapura SG</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Hosting PHP</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Hosting Wordpress</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Hosting Laravel</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Tutorial
+                        </h6>
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0)">Knowledgebase</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Blog</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Cara Pembayaran</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Tentang Kami
+                        </h6>
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0)">Tim Niagahoster</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Karir</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Events</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Penawaran &amp; Promo Spesial</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Kontak Kami</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Kenapa Pilih Niagahoster
+                        </h6>
+                        <ul>
+                            <li>
+                                <a href="javascript:void(0)">Support Terbaik</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Garansi Harga Termurah</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Domain Gratis Selamanya</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Datacenter Hosting Terbaik</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)">Review Pelanggan</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-12">
+                    <div class="footer__widget">
+                        <h6 class="footer__widget__title">
+                            Newsletter
+                        </h6>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <form class="footer__widget__newsletter">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email" required>
+                                        <button class="btn btn-primary" type="submit">Berlangganan</button>
+                                    </div>
+                                    <p class="footer__widget__help-text">Dapatkan promo dan konten menarik dari penyedia hosting terbaik Anda.</p>
+                                </form>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="footer__widget__socials">
+                                    <a href="javascript:void(0)">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                    <a href="javascript:void(0)">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                    <a href="javascript:void(0)">
+                                        <i class="fab fa-google-plus-g"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-lg-12">
+                    <div class="footer__widget mb-0">
+                        <h6 class="footer__widget__title">
+                            Pembayaran
+                        </h6>
+                        <div class="footer__widget__payments">
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/bca.svg') }}" alt="BCA">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/mandiriclickpay.svg') }}" alt="Mandiri Clickpay">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/bni.svg') }}" alt="BNI">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/visa.svg') }}" alt="Visa">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/mastercard.svg') }}" alt="Mastercard">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/atmbersama.svg') }}" alt="ATM Bersama">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/permatabank.svg') }}" alt="Permata Bank">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/prima.svg') }}" alt="Prima">
+                            </div>
+                            <div class="footer__widget__payments__item">
+                                <img src="{{ asset('assets/img/banks/alto.svg') }}" alt="Alto">
+                            </div>
+                        </div>
+                        <p class="footer__widget__help-text mt-2">Aktivasi instan dengan e-Payment. Hosting dan domain langsung aktif!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer__bottom">
+            <div class="row">
+                <div class="col-md-8">
+                    <p>
+                        <small>Copyright &copy;2016 Niagahoster | Hosting powered by PHP7, CloudLinux, CloudFlare, BitNinja and DC Biznet Technovillage Jakarta<br>
+                        Cloud VPS Murah powered by Webuzo Softaculous, Intel SSD and cloud computing technology</small>
+                    </p>
+                </div>
+                <div class="col-md-auto ml-auto">
+                    <p>
+                        <a href="javascript:void(0)">Syarat dan Ketentuan</a> | <a href="javascript:void(0)">Kebijakan Privasi</a>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
